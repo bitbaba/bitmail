a71b3098b9acaf7089d6a667ce39c576414edc31
you <you@a.com>
2016-05-31 13:35:22 +0800

    qt: update login dialog;

--- qt/logindialog.cpp
+++ qt/logindialog.cpp
@@ -1,16 +1,41 @@
 #include "logindialog.h"
 #include "ui_logindialog.h"
 
+#include <QPainter>
+
 LoginDialog::LoginDialog(QWidget *parent) :
     QDialog(parent),
     ui(new Ui::LoginDialog)
 {
     ui->setupUi(this);
     QLabel * lbLogo = findChild<QLabel *>("lbLogo");
-    lbLogo->setPixmap(QPixmap(":/images/bkgrd.png"));
+    QPixmap imgLogo = QPixmap(":/images/bkgrd.png");
+    QPainter pencil;
+    pencil.begin(&imgLogo);
+    pencil.setPen(Qt::red);
+    pencil.setFont(QFont("Arial", 12));
+    pencil.drawText(125, 20, 300, 100, Qt::AlignCenter, tr("BitMail Qt Client"));
+    pencil.end();
+    lbLogo->setPixmap(imgLogo);
+
+    m_lePassphrase = findChild<QLineEdit*>("lePassphrase");
+    m_cbEmail = findChild<QComboBox*>("cbEmail");
+
+    m_cbEmail->addItem(QIcon(":/images/head.png"), "alice@example.net");
+    m_cbEmail->addItem(QIcon(":/images/head.png"), "bob@example.net");
 }
 
 LoginDialog::~LoginDialog()
 {
     delete ui;
+}
+
+void LoginDialog::on_cmdCreate_clicked()
+{
+
+}
+
+void LoginDialog::on_cbEmail_currentIndexChanged(const QString &arg1)
+{
+    (void )arg1;
 }
--- qt/logindialog.h
+++ qt/logindialog.h
@@ -7,6 +7,9 @@ namespace Ui {
 class LoginDialog;
 }
 
+class QComboBox;
+class QLineEdit;
+
 class LoginDialog : public QDialog
 {
     Q_OBJECT
@@ -15,8 +18,16 @@ public:
     explicit LoginDialog(QWidget *parent = 0);
     ~LoginDialog();
 
+private slots:
+    void on_cmdCreate_clicked();
+
+    void on_cbEmail_currentIndexChanged(const QString &arg1);
+
 private:
     Ui::LoginDialog *ui;
+private:
+    QComboBox * m_cbEmail;
+    QLineEdit * m_lePassphrase;
 };
 
 #endif // LOGINDIALOG_H
