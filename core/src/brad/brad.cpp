70d1c06882dbddbd185c1985f9809aa40c9772b5
bitbaba <imzhhwu@gmail.com>
2016-07-01 17:46:13 +0800

    brad: add

---
+++ core/src/brad/brad.cpp
@@ -0,0 +1,120 @@
+/**
+*
+* Forked from https://github.com/lesmon/thd
+* following is original license
+*
+*
+Copyright (c) 2015, 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "brad.h"
+
+#if defined(WIN32)
+#   define CLOSE(fd) closesocket(fd)
+#else
+#   define CLOSE(fd) do {					\
+		while (close((fd)) == -1 && errno == EINTR)	\
+			;					\
+	} while (0)
+#endif
+
+Brac::Brac(int sockfd)
+{
+
+}
+Brac::~Brac()
+{
+
+}
+
+Brad::Brad(OnBradStatus cbStatus, OnBradClientConnect cbClient, void * userptr)
+: m_statusHandler(cbStatus)
+, m_clientHandler(cbClient)
+, m_userptr(userptr)
+{
+
+}
+
+int Brad::Startup(unsigned short port)
+{
+	struct sockaddr_in sin;
+
+	m_serv = socket(AF_INET, SOCK_STREAM, 0);
+	if (m_serv == -1){
+		printf("socket error");
+		return BradErrorSocket;
+	}
+
+	memset(&sin, 0, sizeof(sin));
+	sin.sin_family = AF_INET;
+	sin.sin_port = htons(port);
+	sin.sin_addr.s_addr = INADDR_ANY;
+
+	if (bind(m_serv, (const struct sockaddr*)&sin, sizeof(sin)) == -1){
+		printf("bind error");
+		return BradErrorBind;
+	}
+
+	if (listen(m_serv, SOMAXCONN) == -1){
+		printf("listen error");
+		return BradErrorListen;
+	}
+
+	while (true) {
+		struct sockaddr_in addr;
+		memset(&addr, 0, sizeof(addr));
+		socklen_t len = sizeof(addr);
+		int client = accept(m_serv, (struct sockaddr*)&addr, &len);
+		if (client == -1) {
+			break;
+		}
+		if (m_clientHandler){
+			Brac * c = new Brac(client);
+			m_clientHandler(c, m_userptr);
+		}
+	}
+
+	if (m_statusHandler){
+		m_statusHandler(BradStatusShutdown, m_userptr);
+	}
+
+	return BradErrorOk;
+}
+
+int Brad::CloseClient(Brac * c)
+{
+	if (c){
+		delete c;
+		c = NULL;
+	}
+	return 0;
+}
+
+int Brad::Shutdown()
+{
+	CLOSE(m_serv);
+	m_serv = InvalidSocket;
+	return 0;
+}
---
+++ core/src/brad/brad.h
@@ -0,0 +1,137 @@
+/**
+*
+* Forked from https://github.com/lesmon/thd
+* following is original license
+*
+*
+Copyright (c) 2015, 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <sys/types.h>
+
+#if defined(WIN32)
+#   include <winsock2.h>
+typedef int socklen_t;
+#else
+#   include <err.h>
+#   include <netdb.h>
+#   include <sys/socket.h>
+#   include <netinet/in.h>
+#endif
+
+#include <stdbool.h>
+#include <string.h>
+
+#include <string>
+
+/**
+ * identifier of bra
+ * hex present of sha256 hash of bra
+ */
+typedef std::string BraId;
+
+/**
+ * Invalid socket definition
+ */
+#define InvalidSocket (-1)
+
+/**
+ * Bra type
+ */
+enum Brat{
+	brat_undef   = 0,
+	brat_bitmail = 1,
+	brat_image   = 2,
+	brat_sound   = 3,
+	brat_video   = 4,
+	brat_live    = 5,
+};
+
+enum BradStatus{
+	BradStatusOk = 0,
+	BradStatusStartup = 1,
+	BradStatusShutdown = 2,
+};
+
+enum BradError{
+	BradErrorOk = 0,
+	BradErrorBind = 1,
+	BradErrorListen = 2,
+	BradErrorSocket = 3,
+};
+
+class Brac;
+class Brad;
+
+/**
+ * Callbacks of status
+ */
+typedef int (* OnBradStatus)(BradStatus ec, void * userptr);
+
+typedef int (* OnBradClientConnect)(Brac * c, void * userptr);
+
+
+/**
+ * Definition:
+ * BRA: Bitmail Resource Attachment
+ *
+ */
+class Bra{
+public:
+	Bra();
+	~Bra();
+public:
+	BraId id() const;
+};
+
+/**
+ * Bra client class
+ */
+class Brac{
+public:
+	Brac(int sockfd);
+	~Brac();
+public:
+	int Get(BraId id, const std::string & path);
+	int Put(Brat t, const std::string & path);
+};
+
+/**
+ * Bra daemon class
+ */
+class Brad{
+private:
+	int m_serv;
+	OnBradStatus m_statusHandler;
+	OnBradClientConnect m_clientHandler;
+	void * m_userptr;
+public:
+	Brad(OnBradStatus cbStatus, OnBradClientConnect cbClient, void * userptr);
+	~Brad();
+public:
+	int Startup(unsigned short port);
+	int CloseClient(Brac * c);
+	int Shutdown();
+};
