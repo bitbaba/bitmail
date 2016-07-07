0f70d9d9dc649912d871311e27b69c79248b4d55
bitbaba <imzhhwu@gmail.com>
2016-07-07 11:49:41 +0800

    android: add core java classes.

---
+++ android/core/src/com/bitbaba/BitMail.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bitbaba;
+
+/**
+ * @author Administrator
+ *
+ */
+public class BitMail {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ android/core/src/com/bitbaba/X509Cert.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.bitbaba;
+
+/**
+ * @author Administrator
+ *
+ */
+public class X509Cert {
+
+}
