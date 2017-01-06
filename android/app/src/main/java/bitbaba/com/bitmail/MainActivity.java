dc7b708eb6395bcac6a3157bd75f2164330d39f4
harrywu <imharrywu@gmail.com>
2017-01-06 16:33:22 +0800

    android: add android API12 framework

---
+++ android/app/src/main/java/bitbaba/com/bitmail/MainActivity.java
@@ -0,0 +1,13 @@
+package bitbaba.com.bitmail;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
