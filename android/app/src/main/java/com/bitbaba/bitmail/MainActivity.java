ad6e7a51af374225c0fc4cd123a5a4586a8716a9
harrywu <imharrywu@gmail.com>
2017-01-24 14:31:28 +0800

    android: io in activity;

--- android/app/src/main/java/com/bitbaba/bitmail/MainActivity.java
+++ android/app/src/main/java/com/bitbaba/bitmail/MainActivity.java
@@ -1,10 +1,13 @@
 package com.bitbaba.bitmail;
 
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.bitbaba.core.*;
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.ArrayList;
@@ -15,22 +18,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        X509Cert xcert1 = new X509Cert("nick1", "nick1@example.com", 1024);
-        X509Cert xcert2 = new X509Cert("nick2", "nick2@example.com", 1024);
-
-        Log.d("BitMail", xcert1.GetCertificate());
-        ArrayList<String> certs = new ArrayList<String>();
-        certs.add(xcert1.GetCertificate());
-        certs.add(xcert2.GetCertificate());
-
-        //HashMap<String, String> result = xcert1.Verify(xcert1.Decrypt(xcert1.Encrypt(xcert1.Sign("Hello, World"))));
-        HashMap<String, String> result = xcert1.Verify(xcert1.Decrypt(X509Cert.MEncrypt(certs, xcert1.Sign("Hello, World"))));
-
-        if (result != null){
-            for (Entry<String, String> elt : result.entrySet()){
-                Log.d("BitMail", elt.getKey());
-                Log.d("BitMail", elt.getValue());
-            }
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectDiskReads()
+                .detectDiskWrites().detectNetwork().penaltyLog().build());
+        StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectLeakedSqlLiteObjects()
+                .detectLeakedClosableObjects().penaltyLog().penaltyDeath().build());
+        try {
+            BitMail.main(null);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
