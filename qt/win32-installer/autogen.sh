7cfa80bbf16776a9617422d2b0c248b1f893f20a
harrywu <imharrywu@gmail.com>
2017-05-04 16:46:36 +0800

    qt: installer script;

--- qt/win32-installer/autogen.sh
+++ qt/win32-installer/autogen.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #mingw-get.exe install msys-unzip
-
 NSIS_VERSION="2.51"
 NSIS_RELEASE="nsis-$NSIS_VERSION"
 NSIS_BALL="nsis-$NSIS_VERSION.zip"
@@ -11,7 +10,11 @@ CURL_OPT="-k"
 #CURL_OPT="-k --proxy socks5://127.0.0.1:1080/"
 
 BITMAIL_QT_ROOT="D:\\workspace\\github\\bitmail\\qt"
-BITMAIL_QT_RELEASE_ROOT="D:\\workspace\\github\\bitmail\\qt\\build\\release"
+BITMAIL_DEPENDS_ROOT="D:\\workspace\\github\\bitmail\\depends"
+BITMAIL_CORE_ROOT="D:\\workspace\\github\\bitmail\\core"
+BITMAIL_QT_BUILD_ROOT="$BITMAIL_QT_ROOT\\build"
+BITMAIL_QT_RELEASE_ROOT="$BITMAIL_QT_BUILD_ROOT\\release"
+BITMAIL_QT_INSTALLER_ROOT="$BITMAIL_QT_ROOT\\win32-installer"
 
 QT_LICENSE_ROOT="C:\\Qt\\Qt5.7.0\\Licenses"
 QT_DIST_ROOT="C:\\Qt\\Qt5.7.0\\5.7\\mingw53_32\\bin"
@@ -25,8 +28,22 @@ QT_PLUGINS_ROOT="C:\\Qt\\Qt5.7.0\\5.7\mingw53_32\\plugins"
 #QT_TOOLS_ROOT="C:\\Qt\\Qt5.4.1\\Tools\\mingw530_32\\bin"
 #QT_PLUGINS_ROOT="C:\\Qt\\Qt5.4.1\\5.4\mingw491_32\\plugins"
 
+cd $BITMAIL_DEPENDS_ROOT
+./openssl.sh && ./cur.sh && ./libmicrohttpd.sh && ./miniupnpc.sh && ./qrencode.sh
+
+cd $BITMAIL_CORE_ROOT
+./autogen.sh
+
+cd $BITMAIL_QT_ROOT
+./vergen.sh 
+
+cd $BITMAIL_QT_BUILD_ROOT
+qmake -spec win32-g++ ../bitmail.pro
+mingw32-make clean && mingw32-make 
+
+cd $BITMAIL_QT_INSTALLER_ROOT
 if ! [ -f "$NSIS_BALL" ]; then
-	../../out/bin/curl $CURL_OPT -o $NSIS_BALL $NSIS_LINK
+	$BITMAIL_QT_ROOT/out/bin/curl $CURL_OPT -o $NSIS_BALL $NSIS_LINK
 fi
 
 if ! [ -f "$NSIS_CMD" ]; then
