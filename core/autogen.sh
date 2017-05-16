12e1a95217e177a04f1d89e7429ead82186bf27e
harrywu <imharrywu@gmail.com>
2017-05-16 17:15:23 +0800

    core: build script;

--- core/autogen.sh
+++ core/autogen.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
-if [ x$(uname -s) = x'Darwin' ]; then
-	alias readlink='greadlink'
-fi
-
-BitmailCoreHome="$(dirname $(readlink -f "$0") )"
-#BitmailCoreHome=~/workspace/github/bitmail/core #"$(dirname $(readlink -f "$0") )"
-BitmailHome="$(dirname $BitmailCoreHome)"
-#BitmailHome=~/workspace/github/bitmail #"$(dirname $BitmailCoreHome)"
-BitmailOutHome=$BitmailHome/out
-
-cd $BitmailCoreHome
-
-if [[ $(uname -s) != "MINGW"* ]]; then
-	PICFLAGS='-fPIC -fpic'
-fi
+BitmailCoreHome=${PWD}
+BitmailCoreDependsHome=${BitmailCoreHome}/../out/depends
+BitmailCoreOutHome=${BitmailCoreHome}/../out/core
 
 # Version
 echo \#define BITMAIL_CORE_GIT_VERSION \"$(git log -1 .| grep commit | cut -d " " -f 2)\" > $BitmailCoreHome/include/bitmailcore/version.h
 
-DebugFlags="-g -O0"
-
 aclocal -I m4 \
 && autoconf \
 && automake -fac \
-&& ./configure --prefix="$BitmailOutHome" \
+&& ./configure --prefix="$BitmailCoreOutHome" \
     --enable-static=yes  \
     --enable-shared=no \
-    CXXFLAGS="${DebugFlags} ${PICFLAGS}" \
-    CPPFLAGS="-I$BitmailOutHome/include" \
-    LDFLAGS=" -L$BitmailOutHome/lib" \
-&& make && make install && make clean
+    CXXFLAGS="-fPIC -fpic" \
+    CPPFLAGS="-I$BitmailCoreDependsHome/include" \
+    LDFLAGS=" -L$BitmailCoreDependsHome/lib" \
+&& make \
+&& make install \
+&& make clean
 
 
