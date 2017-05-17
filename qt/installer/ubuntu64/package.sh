6b16816ad2d6ab1b7d3ace719e611833a74bc1a5
imharrywu <imharrywu@aol.com>
2017-05-17 18:44:13 +0800

    qt: package on ubuntu

--- qt/installer/ubuntu64/package.sh
+++ qt/installer/ubuntu64/package.sh
@@ -4,6 +4,8 @@ QtBinRoot=/opt/Qt5.8.0/5.8/gcc_64/bin
 
 QtLibRoot=/opt/Qt5.8.0/5.8/gcc_64/lib
 
+QtPluginsRoot=/opt/Qt5.8.0/5.8/gcc_64/plugins
+
 BitmailRepoRoot=${PWD}/../../..
 
 BitmailQtRoot=${BitmailRepoRoot}/qt
@@ -22,15 +24,36 @@ ${COPY} ${BitmailQtBuildRoot}/bitmail   ${SandBox}
 ${COPY} ${BitmailQtBuildRoot}/audiorecorder ${SandBox}
 ${COPY} ${BitmailQtBuildRoot}/camera   ${SandBox}
 ${COPY} ${BitmailQtBuildRoot}/screenshot   ${SandBox}
+
 ${COPY} ${QtLibRoot}/libQt5Widgets.so.5 ${SandBox}
 ${COPY} ${QtLibRoot}/libQt5Gui.so.5     ${SandBox}
 ${COPY} ${QtLibRoot}/libQt5Network.so.5 ${SandBox}
 ${COPY} ${QtLibRoot}/libQt5Core.so.5    ${SandBox}
+${COPY} ${QtLibRoot}/libQt5XcbQpa.so.5    ${SandBox}
+${COPY} ${QtLibRoot}/libQt5DBus.so.5    ${SandBox}
+${COPY} ${QtLibRoot}/libQt5MultimediaWidgets.so.5    ${SandBox}
+${COPY} ${QtLibRoot}/libQt5Multimedia.so.5    ${SandBox}
+${COPY} ${QtLibRoot}/libQt5OpenGL.so.5    ${SandBox}
+
 ${COPY} ${QtLibRoot}/libicui18n.so.56   ${SandBox}
 ${COPY} ${QtLibRoot}/libicuuc.so.56     ${SandBox}
 ${COPY} ${QtLibRoot}/libicudata.so.56   ${SandBox}
+
+${COPY} ${QtPluginsRoot}   ${SandBox}
+
 ${COPY} ${BitmailQtRoot}/res   ${SandBox}
+
 mkdir -p ${SandBox}/locale
 ${COPY} ${BitmailQtRoot}/locale/*.qm   ${SandBox}/locale
 
+cat <<heredoc >${SandBox}/run.sh
+#!/bin/bash
+QT_PLUGIN_PATH=\${PWD}/plugins LD_LIBRARY_PATH=\${PWD}:\${LD_LIBRARY_PATH} ./bitmail
+heredoc
+
+chmod a+x ${SandBox}/run.sh
+
 tar czf ${package_name}.tar.gz ${package_name}
+
+
+
