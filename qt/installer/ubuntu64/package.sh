98325e3a7a0643dbafeb76d0c9edcd5ea6a70e61
imharrywu <imharrywu@aol.com>
2017-05-17 12:11:26 +0800

    qt: ubuntu pacakge

---
+++ qt/installer/ubuntu64/package.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+QtBinRoot=/opt/Qt5.8.0/5.8/gcc_64/bin
+
+QtLibRoot=/opt/Qt5.8.0/5.8/gcc_64/lib
+
+BitmailRepoRoot=${PWD}/../../..
+
+BitmailQtBuildRoot=${BitmailRepoRoot}/out/qt
+
+SandBox=${PWD}/sandbox
+
+mkdir -p ${SandBox}
+
+COPY="cp -rfL"
+
+${COPY} ${BitmailQtBuildRoot}/bitmail   ${SandBox}
+${COPY} ${BitmailQtBuildRoot}/audiorecorder ${SandBox}
+${COPY} ${BitmailQtBuildRoot}/camera   ${SandBox}
+${COPY} ${BitmailQtBuildRoot}/screenshot   ${SandBox}
+${COPY} ${QtLibRoot}/libQt5Widgets.so.5 ${SandBox}
+${COPY} ${QtLibRoot}/libQt5Gui.so.5     ${SandBox}
+${COPY} ${QtLibRoot}/libQt5Network.so.5 ${SandBox}
+${COPY} ${QtLibRoot}/libQt5Core.so.5    ${SandBox}
+${COPY} ${QtLibRoot}/libicui18n.so.56   ${SandBox}
+${COPY} ${QtLibRoot}/libicuuc.so.56     ${SandBox}
+${COPY} ${QtLibRoot}/libicudata.so.56   ${SandBox}
