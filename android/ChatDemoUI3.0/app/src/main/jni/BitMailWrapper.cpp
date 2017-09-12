4f6c179783d017cc8f7ab408c36ef44febf879ec
harrywu <imharrywu@gmail.com>
2017-09-12 19:13:12 +0800

    for openssl;

--- android/ChatDemoUI3.0/app/src/main/jni/BitMailWrapper.cpp
+++ android/ChatDemoUI3.0/app/src/main/jni/BitMailWrapper.cpp
@@ -3,12 +3,14 @@
 //
 #include <string>
 #include <cstdint>
+#include <android/log.h>
 #include "BitMailWrapper.h"
 
 BitMailInst::BitMailInst() {
     int x = 0;
     x = 1;
     std::string y = "hello, world";
+    __android_log_print(ANDROID_LOG_ERROR, "BitMailWrapper", "Message: %s", "Hello");
 }
 
 BitMailInst::~BitMailInst() {
