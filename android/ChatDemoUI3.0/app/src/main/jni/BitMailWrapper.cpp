36b4e7fa76b0bf0e312f4b4d446c1ac1f9597fa6
harrywu <imharrywu@gmail.com>
2017-09-12 14:48:07 +0800

    add JNI wrapper class;

---
+++ android/ChatDemoUI3.0/app/src/main/jni/BitMailWrapper.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Administrator on 9/11/2017.
+//
+
+#include "BitMailWrapper.h"
---
+++ android/ChatDemoUI3.0/app/src/main/jni/BitMailWrapper.h
@@ -0,0 +1,8 @@
+//
+// Created by Administrator on 9/11/2017.
+//
+
+#ifndef CHATDEMOUI3_0_BITMAILWRAPPER_H
+#define CHATDEMOUI3_0_BITMAILWRAPPER_H
+
+#endif //CHATDEMOUI3_0_BITMAILWRAPPER_H
