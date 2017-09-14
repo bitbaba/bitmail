e989c658147ba378bb4b8f4452dac386125ed6aa
harrywu <imharrywu@gmail.com>
2017-09-14 19:00:47 +0800

    ignore

--- android/easeui/easeui/src/main/jni/BitMailWrapper.cpp
+++ android/easeui/easeui/src/main/jni/BitMailWrapper.cpp
@@ -12,24 +12,12 @@
 #include "x509cert.h"
 
 JNIEXPORT jstring JNICALL
-Java_com_hyphenate_chatuidemo_DemoApplication_NativeJsonRPC(JNIEnv *env
-                                                            , jobject instance
-                                                            , jstring payload_) {
-
-    {
-        OPENSSL_load_builtin_modules();
-        ERR_load_crypto_strings();
-        OpenSSL_add_all_ciphers();
-        OpenSSL_add_all_algorithms();
-        OpenSSL_add_all_digests();
-    }
+Java_com_hyphenate_easeui_EaseUI_NativeJsonRPC(JNIEnv *env, jclass type, jstring payload_) {
     const char *payload = env->GetStringUTFChars(payload_, 0);
 
-    CX509Cert xcert;
-    xcert.Create(1024, "Harry Bot", "751302230@qq.com", "secret");
-    std::string sig = xcert.Sign(payload);
+    // TODO
 
     env->ReleaseStringUTFChars(payload_, payload);
 
-    return env->NewStringUTF(sig.c_str());
+    return env->NewStringUTF("");
 }
--- android/easeui/easeui/src/main/jni/BitMailWrapper.h
+++ android/easeui/easeui/src/main/jni/BitMailWrapper.h
@@ -12,22 +12,10 @@ extern "C" {
 #endif
 
 JNIEXPORT jstring JNICALL
-Java_com_hyphenate_chatuidemo_DemoApplication_NativeJsonRPC(JNIEnv *env, jobject instance, jstring payload_) ;
+Java_com_hyphenate_easeui_EaseUI_NativeJsonRPC(JNIEnv *env, jclass type, jstring payload_);
 
 #ifdef __cplusplus
 };
 #endif
 
 #endif //CHATDEMOUI3_0_BITMAILWRAPPER_H
-
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_com_hyphenate_easeui_EaseUI_NativeJsonRPC(JNIEnv *env, jclass type, jstring payload_) {
-    const char *payload = env->GetStringUTFChars(payload_, 0);
-
-    // TODO
-
-    env->ReleaseStringUTFChars(payload_, payload);
-
-    return env->NewStringUTF(returnValue);
-}
